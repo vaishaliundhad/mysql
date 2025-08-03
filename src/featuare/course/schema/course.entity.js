e9f700a42a3300eb7c4a75c51371fd9883c0656a
bansi43 <bansiundhad34@gmail.com>
2025-08-03 21:29:27 +0530

    course.entity

---
+++ src/featuare/course/schema/course.entity.js
@@ -0,0 +1,47 @@
+import { DataTypes, Model } from "sequelize";
+
+export class courseEntity extends Model{
+    static initModel (sequelize){
+        courseEntity.init({
+            id:{
+                type:DataTypes.UUID,
+                defaultValue:DataTypes.UUIDV4,
+                primaryKey:true
+            },
+            
+            title:{
+                type:DataTypes.STRING,
+                allowNull:false
+            },
+            description:{
+                type:DataTypes.STRING,
+                allowNull:false
+            },
+            price:{
+                type:DataTypes.FLOAT,
+                allowNull:false
+            },
+            duration:{
+                type:DataTypes.STRING,
+                allowNull:false
+            },
+            level:{
+                type:DataTypes.ENUM("beginner" , "intermediate" , "advanced"),
+                allowNull:false
+            },
+            isActive:{
+                type:DataTypes.BOOLEAN,
+                allowNull:false
+            }
+
+
+        },
+        sequelize,
+        modelName ="courseEntity",
+        tableName ="courses"
+        
+    );
+
+     return courseEntity;
+    }
+}
